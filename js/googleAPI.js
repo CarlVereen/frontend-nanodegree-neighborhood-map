04b83ff49b5d0c3af6991e4cca1b699a00180dc2
Carl Vereen <carlvereen@gmail.com>
2015-08-19 06:57:46 -0600

    setup map navigation buttons

--- js/googleAPI.js
+++ js/googleAPI.js
@@ -17,44 +17,86 @@ function CenterControl(controlDiv, map, center) {
   control.center_ = center;
   controlDiv.style.clear = 'both';
 
-  // Set CSS for the control border
-  var goCenterUI = document.createElement('div');
-  goCenterUI.id = 'goCenterUI';
-  goCenterUI.title = 'Click to recenter the map';
-  controlDiv.appendChild(goCenterUI);
+	// Set CSS for the control border
+  var goFoodUI = document.createElement('div');
+  goFoodUI.id = 'goFoodUI';
+  goFoodUI.title = 'Click to check out restaurants';
+  controlDiv.appendChild(goFoodUI);
 
   // Set CSS for the control interior
-  var goCenterText = document.createElement('div');
-  goCenterText.id = 'goCenterText';
-  goCenterText.innerHTML = 'Center Map';
-  goCenterUI.appendChild(goCenterText);
+  var goFoodText = document.createElement('div');
+  goFoodText.id = 'goFoodText';
+  goFoodText.innerHTML = 'Restaurants';
+  goFoodUI.appendChild(goFoodText);
+
+	// Set CSS for the control border
+  var goMuseumUI = document.createElement('div');
+  goMuseumUI.id = 'goMuseumUI';
+  goMuseumUI.title = 'Click to check out Museums';
+  controlDiv.appendChild(goMuseumUI);
+
+  // Set CSS for the control interior
+  var goMuseumText = document.createElement('div');
+  goMuseumText.id = 'goMuseumText';
+  goMuseumText.innerHTML = 'Museums';
+  goMuseumUI.appendChild(goMuseumText);
 
   // Set CSS for the setCenter control border
-  var setCenterUI = document.createElement('div');
-  setCenterUI.id = 'setCenterUI';
-  setCenterUI.title = 'Click to change the center of the map';
-  controlDiv.appendChild(setCenterUI);
+  var goHotelUI = document.createElement('div');
+  goHotelUI.id = 'goHotelUI';
+  goHotelUI.title = 'Click to change the center of the map';
+  controlDiv.appendChild(goHotelUI);
 
   // Set CSS for the control interior
-  var setCenterText = document.createElement('div');
-  setCenterText.id = 'setCenterText';
-  setCenterText.innerHTML = 'Set Center';
-  setCenterUI.appendChild(setCenterText);
+  var goHotelText = document.createElement('div');
+  goHotelText.id = 'goHotelText';
+  goHotelText.innerHTML = 'Hotels';
+  goHotelUI.appendChild(goHotelText);
+
+	// Set CSS for the control border
+	var goCenterUI = document.createElement('div');
+	goCenterUI.id = 'goCenterUI';
+	goCenterUI.title = 'Click to recenter the map';
+	controlDiv.appendChild(goCenterUI);
+
+	// Set CSS for the control interior
+	var goCenterText = document.createElement('div');
+	goCenterText.id = 'goCenterText';
+	goCenterText.innerHTML = 'Center on Tokyo';
+	goCenterUI.appendChild(goCenterText);
+
+// =======================================================================
+//Event listeners
 
   // Set up the click event listener for 'Center Map': Set the center of the map
   // to the current center of the control.
   goCenterUI.addEventListener('click', function() {
-    var currentCenter = control.getCenter();
-    map.setCenter(currentCenter);
+    map.setCenter(tokyo);
   });
 
-  // Set up the click event listener for 'Set Center': Set the center of the
-  // control to the current center of the map.
-  setCenterUI.addEventListener('click', function() {
-    var newCenter = map.getCenter();
-    control.setCenter(newCenter);
+  // Set up the click event listener for 'Hotel search': add hotel map icons
+  goHotelUI.addEventListener('click', function() {
+    //call yelp api
+		//call forsquare api
+		console.log('click');
+  });
+
+	// Set up the click event listener for 'restaurants': add restaurant map icons
+  goFoodUI.addEventListener('click', function() {
+		//call yelp api
+		//call forsquare api
+		console.log('click');
+  });
+
+  // Set up the click event listener for 'Musems': add museum map icons
+  goMuseumUI.addEventListener('click', function() {
+		//call yelp api
+		//call forsquare api
+		console.log('click');
   });
 }
+
+//======================================================================
 
 /**
  * Define a property to hold the center state.
@@ -79,10 +121,13 @@ CenterControl.prototype.setCenter = function(center) {
 };
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: tokyo
-  });
+	var mapOptions = {
+		center: tokyo,
+		zoom: 10,
+		mapTypeId: google.maps.MapTypeId.TERRAIN,
+		disableDefaultUI: true
+	};
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
   // Create the DIV to hold the control and call the CenterControl() constructor
   // passing in this DIV.
@@ -94,3 +139,5 @@ function initMap() {
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 }
 google.maps.event.addDomListener(window, 'load', initMap);
+
+// ===================================================================
